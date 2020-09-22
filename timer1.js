d958873d9e07ed627b7ade0cb0fab5b3e60e34b4
Soren Nissen <s.nissen.bass@gmail.com>
2020-09-22 20:01:32 +0000

    fixed timer1.js to work properly

--- timer1.js
+++ timer1.js
@@ -1,11 +1,14 @@
 //take in args from CL and slice so we only have desired args in our array
 const args = process.argv.slice(2);
 
+
 // we will receive an array of the alarm times  
 // we need to make the alarm ring based on each of the times provided
 
 for (let time of args) {
-  if (time >= 0 || Number.isNaN(time)) {
+  //convert element to number since they are default strings with process argv
+  time = Number(time);
+  if (time >= 0 || typeof time === Number) {
     setTimeout(() => {
       beep();
     }, time * 1000);
@@ -16,3 +19,6 @@ const beep = function() {
   //beep won't work on my OS 
   process.stdout.write('. BEEP . \n');
 }
+
+console.log(typeof Number(args[0]))
+
